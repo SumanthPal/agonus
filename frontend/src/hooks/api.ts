0be747a4402973945d8994442558af9de64c18c8
SumanthPal <sumanth@pallamreddy.com>
2025-12-05 17:55:06 -0800

    more fixes to url

--- frontend/src/hooks/api.ts
+++ frontend/src/hooks/api.ts
@@ -1,21 +1,9 @@
 //stored shared API logic
 
-export const API_URL = "https://agonus-backend-45256917921.us-west1.run.app";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export function getAuthHeaders(): Record<string, string> {
   const token = localStorage.getItem("token");
   return token ? { Authorization: `Bearer ${token}` } : {};
-}
-
-export function getDefaultHeaders(): Record<string, string> {
-  return {
-    "ngrok-skip-browser-warning": "true",
-  };
-}
-
-export function getAllHeaders(): Record<string, string> {
-  return {
-    ...getDefaultHeaders(),
-    ...getAuthHeaders(),
-  };
 }
